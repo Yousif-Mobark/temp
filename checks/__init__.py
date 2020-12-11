ca801caa05f18d99fdeba712feb1c6515e4df90a
Bashier Elbashier <elbashier97@gmail.com>
2020-12-11 22:12:06 +0200

    VARIOUS

--- checks/__init__.py
+++ checks/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
 
 from . import wizards
-from . import controllers
 from . import models
--- project_agreement/__manifest__.py
+++ project_agreement/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['project','purchase' ,'account_invoicing', 'hr_user_group'],
+    'depends': ['project','purchase' ,'account_invoicing', 'account_budget','hr_user_group'],
 
     # always loaded
     'data': [
