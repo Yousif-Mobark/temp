66e40311835f07031079785f1298d14d836b180f
yousif mobark <yousifmobark@gmail.com>
2020-12-13 20:43:49 +0200

    fix manifest

--- customizations_yousif/__manifest__.py
+++ customizations_yousif/__manifest__.py
@@ -13,7 +13,7 @@
         'data/agreement_code_sequence.xml',
         'data/project_code_sequence.xml',
         'views/project_project_views.xml',
-        'views/res_partner_form.xml'
+        'views/res_partner_form.xml',
         'views/project_agreement_views.xml',
     ],
 }
